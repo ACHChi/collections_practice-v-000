8b385d8f338dcce385e605a4e7b3b89517afc0e3
Chao Hsuan Chi <allen0413@gmail.com>
2018-11-14 04:30:16 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,7 +3,13 @@ def sort_array_asc(array)
 end
 
 def sort_array_desc(array)
-  array.sort do | left, right|
+  array.sort do |left, right|
     right <=> left
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |left, right|
+    left.length <=> right.length
   end
 end
